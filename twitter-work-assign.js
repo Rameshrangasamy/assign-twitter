5b56d4f5817357c0f96790780c41bea2312a844a
Rameshrangasamy <154510843+Rameshrangasamy@users.noreply.github.com>
2023-12-26 13:46:00 +0530

    Update twitter-work-assign.js

--- twitter-work-assign.js
+++ twitter-work-assign.js
@@ -3,7 +3,6 @@ const {open} = require('sqlite')
 const sqlite3 = require('sqlite3')
 const path = require('path')
 const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
 
 const app = express()
 app.use(express.json())
@@ -18,9 +17,9 @@ const intialiazeDbAndServer = async () => {
       filename: dbPath,
       driver: sqlite3.Database,
     })
-    app.listen(3000, () => {
-      console.log('Server Running at http://localhost:3000')
-    })
+    app.listen(3000, () =>
+      console.log('Server Running at http://localhost:3000/'),
+    )
   } catch (error) {
     console.log(`DB Error: ${error.message}`)
     process.exit(1)
@@ -30,35 +29,36 @@ const intialiazeDbAndServer = async () => {
 intialiazeDbAndServer()
 
 //validate password
+
 const validatePassword = password => {
-  return password.length >= 6
+  return password.length > 5
 }
 
 // Register user API
 
-app.post('/register/', async (request, response) => {
+app.post('/register', async (request, response) => {
   const {username, password, name, gender} = request.body
-  const hashedPassowrd = await bcrypt.hash(password, 10)
-  const checkUsernameQuery = `SELECT * FROM user WHERE username = "${username}"`
-  const dbUser = await db.get(checkUsernameQuery)
+  const hashedPassword = await bcrypt.hash(password, 10)
+  const checkUserNameQuery = `SELECT * FROM user WHERE username = '${username}';`
+  const dbUser = await db.get(checkUserNameQuery)
 
   if (dbUser === undefined) {
     const createUserQuery = `
     INSERT INTO
     user(name, username, password, gender)
     VALUES
-    ("${name}", "${username}", "${hashedPassowrd}", "${gender}");`
+    ("${name}", "${username}", "${hashedPassword}", "${gender}");`
 
     if (validatePassword(password)) {
       await db.run(createUserQuery)
-      response.status = 200
+      response.status(200)
       response.send('User created successfully')
     } else {
-      response.status = 400
+      response.status(400)
       response.send('Password is too short')
     }
   } else {
-    response.status = 400
+    response.status(400)
     response.send('User already exists')
   }
 })
