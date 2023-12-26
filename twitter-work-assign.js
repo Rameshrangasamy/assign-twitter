e14d19d8aef49b34ed4cbeb0006474002ed339bb
Rameshrangasamy <154510843+Rameshrangasamy@users.noreply.github.com>
2023-12-26 12:26:57 +0530

    Create twitter-work-assign.js

---
+++ twitter-work-assign.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const {open} = require('sqlite')
+const sqlite3 = require('sqlite3')
+const path = require('path')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const app = express()
+app.use(express.json())
+
+let db = null
+
+const dbPath = path.join(__dirname, 'twitterClone.db')
+
+const intialiazeDbAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    })
+    app.listen(3000, () => {
+      console.log('Server Running at http://localhost:3000')
+    })
+  } catch (error) {
+    console.log(`DB Error: ${error.message}`)
+    process.exit(1)
+  }
+}
+
+intialiazeDbAndServer()
+
+//validate password
+const validatePassword = password => {
+  return password.length >= 6
+}
+
+// Register user API
+
+app.post('/register/', async (request, response) => {
+  const {username, password, name, gender} = request.body
+  const hashedPassowrd = await bcrypt.hash(password, 10)
+  const checkUsernameQuery = `SELECT * FROM user WHERE username = "${username}"`
+  const dbUser = await db.get(checkUsernameQuery)
+
+  if (dbUser === undefined) {
+    const createUserQuery = `
+    INSERT INTO
+    user(name, username, password, gender)
+    VALUES
+    ("${name}", "${username}", "${hashedPassowrd}", "${gender}");`
+
+    if (validatePassword(password)) {
+      await db.run(createUserQuery)
+      response.status = 200
+      response.send('User created successfully')
+    } else {
+      response.status = 400
+      response.send('Password is too short')
+    }
+  } else {
+    response.status = 400
+    response.send('User already exists')
+  }
+})
